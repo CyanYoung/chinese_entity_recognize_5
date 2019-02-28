e1b309885e855c2bd4032cb37a061dd7c2450a86
yangjc <bupt_yjc@163.com>
2019-02-28 15:11:35 +0800

    change to trm

--- nn_arch.py
+++ nn_arch.py
@@ -1,43 +1,67 @@
+import math
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-win_len = 7
-
-
-class Cnn(nn.Module):
-    def __init__(self, embed_mat, class_num):
-        super(Cnn, self).__init__()
-        vocab_num, embed_len = embed_mat.size()
-        self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
-        self.conv = nn.Conv1d(embed_len, 128, kernel_size=win_len, padding=0)
-        self.gate = nn.Conv1d(embed_len, 128, kernel_size=win_len, padding=0)
-        self.la = nn.Sequential(nn.Linear(128, 200),
-                                nn.ReLU())
+class Trm(nn.Module):
+    def __init__(self, embed_mat, pos_mat, class_num, head, stack):
+        super(Trm, self).__init__()
+        self.encode = TrmEncode(embed_mat, pos_mat, head, stack)
         self.dl = nn.Sequential(nn.Dropout(0.2),
                                 nn.Linear(200, class_num))
 
     def forward(self, x):
-        x = self.embed(x)
-        x = x.permute(0, 2, 1)
-        g = torch.sigmoid(self.gate(x))
-        x = self.conv(x)
-        x = x * g
-        x = x.permute(0, 2, 1)
-        x = self.la(x)
+        x = self.encode(x)
+        x = x[:, 0, :]
         return self.dl(x)
 
 
-class Rnn(nn.Module):
-    def __init__(self, embed_mat, class_num):
-        super(Rnn, self).__init__()
+class TrmEncode(nn.Module):
+    def __init__(self, embed_mat, pos_mat, head, stack):
+        super(TrmEncode, self).__init__()
         vocab_num, embed_len = embed_mat.size()
         self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
-        self.ra = nn.LSTM(embed_len, 200, batch_first=True, bidirectional=True)
-        self.dl = nn.Sequential(nn.Dropout(0.2),
-                                nn.Linear(400, class_num))
+        self.pos = pos_mat
+        self.layers = nn.ModuleList([EncodeLayer(embed_len, head) for _ in range(stack)])
 
     def forward(self, x):
+        p = self.pos.repeat(x.size(0), 1, 1)
         x = self.embed(x)
-        h, hc_n = self.ra(x)
-        return self.dl(h)
+        x = x + p
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class EncodeLayer(nn.Module):
+    def __init__(self, embed_len, head):
+        super(EncodeLayer, self).__init__()
+        self.head = head
+        self.qry = nn.Linear(embed_len, 200 * head)
+        self.key = nn.Linear(embed_len, 200 * head)
+        self.val = nn.Linear(embed_len, 200 * head)
+        self.fuse = nn.Linear(200 * head, 200)
+        self.lal = nn.Sequential(nn.Linear(200, 200),
+                                 nn.ReLU(),
+                                 nn.Linear(200, 200))
+        self.lns = nn.ModuleList([nn.LayerNorm(200) for _ in range(2)])
+
+    def mul_att(self, x, y):
+        q = self.qry(y).view(y.size(0), y.size(1), self.head, -1).transpose(1, 2)
+        k = self.key(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        v = self.val(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        d = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(k.size(-1))
+        a = F.softmax(d, dim=-1)
+        c = torch.matmul(a, v).transpose(1, 2)
+        c = c.contiguous().view(c.size(0), c.size(1), -1)
+        return self.fuse(c)
+
+    def forward(self, x):
+        r = x
+        x = self.mul_att(x, x)
+        x = self.lns[0](x + r)
+        r = x
+        x = self.lal(x)
+        return self.lns[1](x + r)
